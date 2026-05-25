579d206d097f7ec825b790bdf05480a7b0d6d137
odelacruz <odelacruz.fierro@gmail.com>
2026-05-25 12:34:31 +0200

    Created project folder architecture.

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+
+
+def main():
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    print("Welcome to Data Challenge Idemia from Télécom Paris")
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
